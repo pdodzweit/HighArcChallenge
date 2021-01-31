e22fc252189c626a14956b8a9c93bf8893f279b8
Paul Dodzweit <paul.dodzweit@gmail.com>
2021-01-31 16:45:30 -0500

    basic setup with three.js is in place, read to work on loading the vertices.

--- index.js
+++ index.js
@@ -1,40 +1,39 @@
 
 function getTerrainImage() {
-    document.getElementById('btn').onclick = function () {
-        var zoom = document.getElementById('zoom').value;
-        var lat = document.getElementById('lat').value
-        var lon = document.getElementById('lon').value
+    var zoom = document.getElementById('zoom').value;
+    var lat = document.getElementById('lat').value
+    var lon = document.getElementById('lon').value
 
-        var tiles = pointToTile(lon, lat, zoom)
-        console.log(tiles)
+    var tiles = pointToTile(lon, lat, zoom)
+    console.log(tiles)
 
-        var src = 'https://api.mapbox.com/v4/mapbox.terrain-rgb/' + String(zoom) + '/' + String(tiles[0]) + '/' + String(tiles[1]) + '.pngraw?access_token=pk.eyJ1IjoicGRvZHp3ZWl0IiwiYSI6ImNra2tjY3dqMDBzODIycHFudHB5c3J0eDgifQ.jKlnMzp8nL4zySUnrQ-vKg';
+    var src = 'https://api.mapbox.com/v4/mapbox.terrain-rgb/' + String(zoom) + '/' + String(tiles[0]) + '/' + String(tiles[1]) + '.pngraw?access_token=pk.eyJ1IjoicGRvZHp3ZWl0IiwiYSI6ImNra2tjY3dqMDBzODIycHFudHB5c3J0eDgifQ.jKlnMzp8nL4zySUnrQ-vKg';
 
-        var canvas = document.getElementById('hiddenCanvas'),
-            context = canvas.getContext('2d');
+    var canvas = document.getElementById('hiddenCanvas'),
+        context = canvas.getContext('2d');
 
-        var base_image = new Image();
-        base_image.src = src;
-        base_image.crossOrigin = "Anonymous";
-        base_image.onload = function () {
-            context.drawImage(base_image, 0, 0);
+    var base_image = new Image();
+    base_image.src = src;
+    base_image.crossOrigin = "Anonymous";
+    base_image.onload = function () {
+        context.drawImage(base_image, 0, 0);
 
-            // Get the CanvasPixelArray from the given coordinates and dimensions.
-            var imgd = context.getImageData(0, 0, 256, 256);
-            var pix = imgd.data;
+        // Get the CanvasPixelArray from the given coordinates and dimensions.
+        var imgd = context.getImageData(0, 0, 256, 256);
+        var pix = imgd.data;
 
-            // Loop over each pixel and invert the color.
-            for (var i = 0, n = pix.length; i < n; i += 4) {
-                pix[i] = 255 - pix[i]; // red
-                pix[i + 1] = 255 - pix[i + 1]; // green
-                pix[i + 2] = 255 - pix[i + 2]; // blue
-                // i+3 is alpha (the fourth element)
-            }
-
-            // Draw the ImageData at the given (x,y) coordinates.
-            context.putImageData(imgd, 0, 0);
+        // Loop over each pixel and invert the color.
+        for (var i = 0, n = pix.length; i < n; i += 4) {
+            pix[i] = 255 - pix[i]; // red
+            pix[i + 1] = 255 - pix[i + 1]; // green
+            pix[i + 2] = 255 - pix[i + 2]; // blue
+            // i+3 is alpha (the fourth element)
         }
+
+        // Draw the ImageData at the given (x,y) coordinates.
+        context.putImageData(imgd, 0, 0);
     }
+
 }
 
 import * as THREE from './three.js/build/three.module.js';
@@ -46,32 +45,36 @@ init();
 
 function init() {
 
-	camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
-	camera.position.z = 1;
+    var width = 500;
+    var height = 500;
+    camera = new THREE.OrthographicCamera(width / - 2, width / 2, height / 2, height / - 2, -2000, 2000);
+    camera.position.z = 1;
 
-	scene = new THREE.Scene();
+    scene = new THREE.Scene();
 
-	geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
-	material = new THREE.MeshNormalMaterial();
+    geometry = new THREE.PlaneGeometry(width / 2, width / 2, 255, 255);
+    material = new THREE.MeshNormalMaterial();
 
-	mesh = new THREE.Mesh( geometry, material );
-	scene.add( mesh );
+    mesh = new THREE.Mesh(geometry, material);
+    scene.add(mesh);
+
+    mesh.rotation.x = -Math.PI / 4;
 
 
     var canvasElm = document.getElementById('canvas');
-    renderer = new THREE.WebGLRenderer( { canvas: canvasElm, antialias: true } );
-	renderer.setSize( window.innerWidth, window.innerHeight );
-	renderer.setAnimationLoop( animation );
-    
-    getTerrainImage();
+    renderer = new THREE.WebGLRenderer({ canvas: canvasElm, antialias: true });
+    renderer.setSize(width, height);
+    renderer.setAnimationLoop(animation);
 
+    document.getElementById('btn').onclick = function () {
+        getTerrainImage();
+    }
 }
 
-function animation( time ) {
+function animation(time) {
 
-	mesh.rotation.x = time / 2000;
-	mesh.rotation.y = time / 1000;
+    mesh.rotation.z = time / 10000;
 
-	renderer.render( scene, camera );
+    renderer.render(scene, camera);
 
 }
